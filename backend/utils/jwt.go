9ec2e10333f73ed153e9a430c3da983dcad4c6c7
Ujjwal Kesarwani <ujjwalkes19@gmail.com>
2026-03-03 16:18:44 +0530

    style: format code for consistency and readability in jwt.go

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -1,29 +1,36 @@
 package utils
+
 import (
 	"errors"
-    "os"
-    "time"
+	"os"
+	"time"
 
-    "github.com/golang-jwt/jwt/v5"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func GenerateJWT(userID string) (string, error) {
-    claims := jwt.MapClaims{
-        "user_id": userID,
-        "exp":     time.Now().Add(24 * time.Hour).Unix(),
-    }
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 }
 
 func ParseJWT(tokenStr string) (string, error) {
-    token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
-        return []byte(os.Getenv("JWT_SECRET")), nil
-    })
-    if err != nil || !token.Valid {
-        return "", errors.New("invalid token")
-    }
-    claims := token.Claims.(jwt.MapClaims)
-    userID := claims["user_id"].(string)
-    return userID, nil
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil || !token.Valid {
+		return "", errors.New("invalid token")
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", errors.New("invalid token claims")
+	}
+	userID, ok := claims["user_id"].(string)
+	if !ok {
+		return "", errors.New("invalid user_id in token")
+	}
+	return userID, nil
 }
