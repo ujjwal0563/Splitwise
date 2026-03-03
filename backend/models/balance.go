d79db2a76de03b8f21c46042520c84c562275bdd
Ujjwal Kesarwani <ujjwalkes19@gmail.com>
2026-03-03 16:21:38 +0530

    refactor: clean up BalanceDetail struct and remove unused NetBalance type

--- backend/models/balance.go
+++ backend/models/balance.go
@@ -1,14 +1,7 @@
 package models
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-type NetBalance struct {
-	UserID primitive.ObjectID  `json:"user_id"`
-	UserName string            `json:"user_name"`
-	Amount float64             `json:"amount"`
-}
+
 type BalanceDetail struct {
-	FromUserID string `json:"from_user"`
-	TOUser string `json:"to_user"`
-	Amount float64 `json:"amount"`
+	FromUserID string  `json:"from_user"`
+	ToUser     string  `json:"to_user"`
+	Amount     float64 `json:"amount"`
 }
